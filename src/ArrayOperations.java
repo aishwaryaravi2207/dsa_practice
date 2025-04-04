fd4a83760d408dea1ac004c70b47a9e5fafcbbaa
Aishwarya Ravichandran <aishwaryaravichandran2207@gmail.com>
2025-04-03 22:45:42 -0400

    adding solution to #1122

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ArrayOperations {
     public static void main(String[] args){
-        int[] nums = {1,2,3,4};
-        int[] input = {2,4,1,1,6,5};
-        System.out.println(runningSum(nums));
-        System.out.println(countHillValley(input));
+        int[] nums = {2,3,1,3,2,4,6,7,9,2,19};
+        int[] input = {2,1,4,3,9,6};
+//        System.out.println(runningSum(nums));
+//        System.out.println(countHillValley(input));
+        System.out.println(relativeSortArray(nums,input).toString());
     }
     /**
      * Leetcode Problem #1480: Running Sum of 1D Array
@@ -73,9 +79,54 @@ public class ArrayOperations {
 
         return count;
     }
+    /**
+     * Leetcode Problem #1122: Relative Sort Array
+     *
+     * This method sorts arr1 such that the relative ordering of items in arr1 are the same as in arr2.
+     * Elements that do not appear in arr2 are placed at the end of arr1 in ascending order.
+     *
+     * Approach:
+     * 1. Count the occurrences of each element in arr1 using a frequency array.
+     * 2. Place elements from arr2 into the result array based on the order in arr2.
+     * 3. Place remaining elements that were not in arr2 into the result array in ascending order.
+     *
+     * Time Complexity: O(N)
+     *    - Counting occurrences and placing elements both involve single passes through arr1 and arr2.
+     *
+     * Space Complexity: O(1)
+     *    - The frequency array uses a fixed amount of space (1001 integers), independent of the input size.
+     *
+     * @param arr1 Input array of integers to be sorted.
+     * @param arr2 Array that defines the order of elements to be sorted in arr1.
+     * @return The sorted array.
+     */
+    public static int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int maxValue = 1000; // Given the constraint 0 ≤ arr1[i], arr2[i] ≤ 1000
+        int[] frequency = new int[maxValue + 1];
 
-    public static int distinctIntegers(int n) {
-        return Math.max(n-1,1);
+        // Count the occurrences of each element in arr1
+        for (int num : arr1) {
+            frequency[num]++;
+        }
+
+        int index = 0;
+
+        // Place elements from arr2 into the result array based on the order in arr2
+        for (int num : arr2) {
+            while (frequency[num] > 0) {
+                arr1[index++] = num;
+                frequency[num]--;
+            }
+        }
+
+        // Place remaining elements that were not in arr2 into the result array in ascending order
+        for (int num = 0; num <= maxValue; num++) {
+            while (frequency[num] > 0) {
+                arr1[index++] = num;
+                frequency[num]--;
+            }
+        }
+
+        return arr1;
     }
-
 }
